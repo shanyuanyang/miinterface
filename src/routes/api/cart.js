6621ecd68036e3e6e93ddaf939799a5b47ff4cb9
syy <1101976384@QQ.COM>
2021-01-26 16:09:35 +0800

    cart:初始化

--- src/routes/api/cart.js
+++ src/routes/api/cart.js
@@ -4,3 +4,13 @@
 */
 
 const router = require('koa-router')()
+const { loginCheck } = require('../../middlewares/loginChecks')
+
+router.prefix('/api/cart')
+
+router.post('/addCart', loginCheck, async (ctx, next) => {
+  
+})
+
+
+module.exports = router
